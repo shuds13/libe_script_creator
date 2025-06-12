6ccfb36a6716cf4f2312fc089c57cbe001519af4
shudson <shudson@anl.gov>
2025-06-12 17:25:15 -0500

    Add basic generator selection

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_file
 import jinja2
 import os
 
 app = Flask(__name__)
+
+@app.route('/generators.json')
+def serve_generators():
+    return send_file('data/generators.json')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -19,6 +23,8 @@ def index():
         num_workers = request.form.get('num_workers', type=int)
         sim_app = request.form.get('sim_app', '')
         input_file = request.form.get('input_file', '')
+        gen_module = request.form.get('gen_module')
+        gen_function = request.form.get('gen_function')
 
         # Worker fields
         nodes = request.form.get('nodes', type=int)
@@ -28,7 +34,9 @@ def index():
         template_path = os.path.join(app.root_path, 'templates', 'run_libe.py.j2')
         with open(template_path) as f:
             template = jinja2.Template(f.read())
-        script = template.render(num_workers=num_workers,
+        script = template.render(gen_module=gen_module,
+                                 gen_function=gen_function,
+                                 num_workers=num_workers,
                                  sim_app=sim_app,
                                  app_ref=app_ref,
                                  input_file=input_file)
