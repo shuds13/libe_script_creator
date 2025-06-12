25e1c2661bac24e93d62873a83e7cde4b25a74a0
shudson <shudson@anl.gov>
2025-06-12 18:44:40 -0500

    Update for render

--- app.py
+++ app.py
@@ -68,4 +68,5 @@ def index():
                            simf_link=simf_link)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
